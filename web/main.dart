f935c3c2e1ad0ddaa41377039dd0359270523e11
Masaki Hikosaka <masaki_hikosaka@outlook.com>
2019-05-12 10:40:48 +0900

    added konami

--- web/main.dart
+++ web/main.dart
@@ -1,5 +1,28 @@
 import 'dart:html';
+import 'package:rxdart/rxdart.dart';
+import "package:collection/collection.dart";
 
 void main() {
-  querySelector('#output').text = 'Your Dart app is running.';
+  const konamiKeyCodes = const <int>[
+    KeyCode.UP,
+    KeyCode.UP,
+    KeyCode.DOWN,
+    KeyCode.DOWN,
+    KeyCode.LEFT,
+    KeyCode.RIGHT,
+    KeyCode.LEFT,
+    KeyCode.RIGHT,
+    KeyCode.B,
+    KeyCode.A
+  ];
+
+  final result = querySelector('#output');
+  final keyUp = Observable<KeyboardEvent>(document.onKeyUp);
+
+  keyUp
+      .map((event) => event.keyCode)
+      .bufferCount(10, 1)
+      .where((lastTenKeyCodes) =>
+          IterableEquality<int>().equals(lastTenKeyCodes, konamiKeyCodes))
+      .listen((_) => result.innerHtml = 'KONAMI!');
 }
